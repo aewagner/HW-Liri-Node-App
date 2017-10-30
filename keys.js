637a9085a8dd5a820b24d84b6811526932682de7
Austin Wagner <austin.e.wagner@gmail.com>
2017-10-29 19:14:07 -0700

    Set up

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'DHRxhK8x5XsDzg2x9eieqOwif',
+  consumer_secret: 'xcXKv4gtthkyIYBDFL0ZT88Of4YCkN81o9S99qKrVMYFaeYUEi',
+  access_token_key: '924816343483813888-NBxarPr0Y8Mv0TwnzG19bohcYvC9XFo',
+  access_token_secret: 'rRvwadSx6TwEm6N1Zycatdxq4pXXmaWdET0qXt0ophrfS',
+}
+
+module.exports = twitterKeys;
